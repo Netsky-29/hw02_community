266e943ffde88d2cf4959e71c9dc3186e20613cb
Грицай Андрей <gricay2011@yandex.ru>
2022-09-17 20:27:22 +0800

    Первая итерация

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,6 +8,7 @@ def index(request):
     context = {
         'posts': posts,
         'title': title,
+        'template': 'posts/index.html',
     }
     return render(request, 'posts/index.html', context)
 
@@ -16,9 +17,10 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     title = f'Записи сообщества {group}'
+    template = 'posts/group_list.html'
     context = {
         'group': group,
         'posts': posts,
         'title': title,
     }
-    return render(request, 'posts/group_list.html', context,)
+    return render(request, template, context,)
