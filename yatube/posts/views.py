f0e93cbe2dccfe00c0f17c7b37bfa5f4c7098c37
Грицай Андрей <gricay2011@yandex.ru>
2022-09-20 18:59:27 +0800

    Третья итерация

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,18 @@
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 
 
 from .models import Post, Group
-from yatube.settings import POST_COUNT
 
 
 def index(request):
-    posts = Post.objects.all()[:POST_COUNT]
+    posts = Post.objects.all()[:settings.POST_COUNT]
     return render(request, 'posts/index.html', {'posts': posts})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.group_posts.all()[:POST_COUNT]
+    posts = group.group_posts.all()[:settings.POST_COUNT]
     return render(
         request,
         'posts/group_list.html',
